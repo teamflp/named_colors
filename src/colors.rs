5cb50851313ef8804f02163ac3caea4face6abd9
teamflp <teamflp@yahoo.it>
2024-10-05 14:51:06 +0200

    Ajout de la librairie named_colors

--- src/colors.rs
+++ src/colors.rs
@@ -3,7 +3,7 @@ use serde_json::Value;
 use std::collections::HashMap;
 use std::error::Error;
 
-const COLORS_JSON_URL: &str = "https://github.com/teamflp/colorsjson/blob/master/named_colors.json"; // URL de ton fichier GitHub
+const COLORS_JSON_URL: &str = "https://github.com/teamflp/named_colors/blob/master/named_colors.json";
 
 pub fn load_colors() -> Result<HashMap<String, Value>, Box<dyn Error>> {
     let response = get(COLORS_JSON_URL)?.text()?;
