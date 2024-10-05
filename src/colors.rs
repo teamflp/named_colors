6b7df1a6c0b93815190e143b472b944c7aa7af87
teamflp <teamflp@yahoo.it>
2024-10-05 22:17:57 +0200

    modif du code

--- src/colors.rs
+++ src/colors.rs
@@ -1,12 +1,20 @@
-use reqwest::blocking::get;
+use reqwest::get;
 use serde_json::Value;
 use std::collections::HashMap;
 use std::error::Error;
 
-const COLORS_JSON_URL: &str = "https://github.com/teamflp/named_colors/blob/master/named_colors.json";
+const COLORS_JSON_URL: &str = "https://raw.githubusercontent.com/teamflp/named_colors/refs/heads/master/named_colors.json";  // Utilise le lien vers le fichier brut
 
-pub fn load_colors() -> Result<HashMap<String, Value>, Box<dyn Error>> {
-    let response = get(COLORS_JSON_URL)?.text()?;
-    let colors: HashMap<String, Value> = serde_json::from_str(&response)?;
+
+// Cette fonction télécharge et charge les couleurs à partir du fichier JSON hébergé.
+pub async fn load_colors() -> Result<HashMap<String, Value>, Box<dyn Error>> {
+    // Requête HTTP pour récupérer le fichier JSON brut
+    let response = get(COLORS_JSON_URL).await?.text().await?;
+    
+    // Parse le contenu JSON et le mappe dans un HashMap
+    let colors: HashMap<String, Value> = serde_json::from_str(&response)
+        .map_err(|err| Box::new(err) as Box<dyn Error>)?;
+    
     Ok(colors)
 }
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@ pub mod colors;
 
 use colors::load_colors;
 
-pub fn get_color_by_name(color_name: &str) -> Option<(u8, u8, u8)> {
-    let color_data = load_colors().unwrap_or_default();
-    if let Some(color) = color_data.get(color_name) {
+// Fonction pour obtenir les valeurs RGB d'une couleur par son nom
+pub async fn get_color_by_name(color_name: &str) -> Option<(u8, u8, u8)> {
+    let color_data = load_colors().await.unwrap_or_default();
+    let color_name = color_name.to_lowercase();  // Convertir en minuscule pour uniformiser la recherche
+    if let Some(color) = color_data.get(&color_name) {
         let r = color["r"].as_u64().unwrap_or(0) as u8;
         let g = color["g"].as_u64().unwrap_or(0) as u8;
         let b = color["b"].as_u64().unwrap_or(0) as u8;
